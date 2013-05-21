31aab70d2896cca7068df806474696feca207c93
dbc-apprentice <apprentice@devbootcamp.com>
2013-05-20 21:14:32 -0500

    working stats page and unique URL

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -9,13 +9,20 @@ get '/game' do
   redirect "/game/#{@game.id}/#{@player1.id}/#{@round_1.id}/#{@player2.id}/#{@round_2.id}"
 end
 
+get '/results/:url' do
+  @game = Game.find_by_unique_url(params[:url])
+  erb :game_result
+end
+
 
 post '/game/:game_id/:player1_id/:round_1_id/:player2_id/:round_2_id/results' do
   puts " "
   puts " "
   puts " "
   p params[:winner]
+  random = ("A".."Z").to_a.sample(6).join('')
   @game = Game.find(params[:game_id])
+  @game.update_attributes(:unique_url => random)
   rounds = @game.rounds
   rounds.each do |round|
     if round.player_id == Player.find_by_username(params[:winner]).id
