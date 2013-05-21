0d5edbfc38ae5b18e09f8e1df5975ba158cd345a
dbc-apprentice <apprentice@devbootcamp.com>
2013-05-20 20:15:42 -0500

    Cleaned up jQuery

--- public/js/application.js
+++ public/js/application.js
@@ -1,51 +1,44 @@
 $(document).ready(function() {
   $(document).keyup(function(e){
-    start = Date.now()
-    if (e.keyCode == 37){
-      var currentlyActive = $('.player_1_track');
-      console.log(currentlyActive);
-      currentlyActive.removeClass('player_1_track');
-      currentlyActive.next().addClass('player_1_track');
-      if ($('#player1_strip td').last().hasClass('player_1_track')) { 
-        alert("Player 1 wins!");
-        var winner_name = $('#player1_strip td').first().text();
-        var time = Date.now() - start;
-        $.post(window.location.pathname+'/results', {'winner': winner_name, 'time': time }, function(r) {
-          $('body').html(r)
-        }); 
-        return false;
-      };
-      return false;
-    }
-
-    if (e.keyCode == 39) {
-      var currentlyActive1 = $('.player_2_track');
-      currentlyActive1.removeClass('player_2_track');
-      currentlyActive1.next().addClass('player_2_track');
-      if ($('#player2_strip td').last().hasClass('player_2_track')) { 
-        alert("Player 2 wins!");
-        var winner_name = $('#player2_strip td').first().text();
-        var time = Date.now() - start;
-        $.post(window.location.pathname+'/results', {'winner': winner_name, 'time': time }, function(r) {
-          $('body').html(r)
-        }); 
-        return false;
-      };
-      console.log("test")
-      return false;
+    var start = Date.now()
+    if (leftArrowPressed(e)) {
+      return advancePlayer(1, start);
+    } 
+    if (rightArrowPressed(e)) {
+      return advancePlayer(2, start);
     };
   });
 });
 
+function leftArrowPressed(e) {
+  if (e.keyCode == 37) {
+    return true;
+  }
+}
 
+function rightArrowPressed(e) {
+  if (e.keyCode == 39) {
+    return true;
+  }
+}
 
-      
+function gameOver(player, player_track, player_strip, start) {
+  if ($(player_strip).last().hasClass(player_track)) {
+    alert('Player ' + player + ' wins!');
+    var winner_name = $(player_strip).first().text();
+    var time = Date.now() - start;
+    $.post(window.location.pathname+'/results', {'winner': winner_name, 'time': time }, function(r) {
+      $('body').html(r)
+    }); 
+  };
+}
 
-    //        if ($('#player1_strip td').last().hasClass('active')) { 
-    //     alert("Player 1 wins!");
-    //     var winner_name = $('#player1_strip td').first()
-    //     var time = Date.now() - start;
-    //     $("form").append("<input type='hidden' name='winner' value='" + winner_id + "'/>").submit();
-    //     location.reload();
-    //   };
-    // }
+function advancePlayer(player, start) {
+  var player_track = 'player_' + player + '_track';
+  var player_strip = '#player' + player + '_strip td';
+  var currentlyActive = $('.' + player_track);
+  currentlyActive.removeClass(player_track);
+  currentlyActive.next().addClass(player_track);
+  gameOver(player, player_track, player_strip, start);
+  return false;
+}
